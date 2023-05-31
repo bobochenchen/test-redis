5b866e7fecbb403472e3140e59b86f2d51333267
chenbo <17673488086@126.com>
2023-05-31 17:52:12 +0800

    #add 添加swagger依赖并测试完成

--- src/main/java/com/chenbo/testredis/TestRedisApplication.java
+++ src/main/java/com/chenbo/testredis/TestRedisApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.ssssssss.magicapi.core.annotation.MagicModule;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TestRedisApplication {
 
     public static void main(String[] args) {
